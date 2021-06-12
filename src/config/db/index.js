55d8c41aaf9da6c71a6647a4352c622484e8cb4c
NguyenTrongHien-1011 <82128664+NguyenTrongHien-1011@users.noreply.github.com>
2021-06-12 13:42:29 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,7 +4,7 @@ async function connect() {
 
    
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/DoAnCoSo1', {
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/DoAnCoSo1', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
          useFindAndModify: false,
